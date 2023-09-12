8ea37c69de2e8061b853d88310bc85e1dcf46dbf
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-09-12 15:57:26 +0200

    js [add] if confronto [add] comments

--- esDadi/script.js
+++ esDadi/script.js
@@ -1,5 +1,15 @@
+const message = document.querySelector('h2');
+
 const numComputer = Math.floor(Math.random() * 6) + 1;
 const numUtente = Math.floor(Math.random() * 6) + 1;
 
 // console.log(numComputer);
 // console.log(numUtente);
+
+if(numComputer < numUtente){
+  message.innerHTML = 'Il vincitore è l\'utente';
+}else if(numComputer === numUtente){
+  message.innerHTML = 'I due numeri sono uguali, nessun vincitore';
+}else{
+  message.innerHTML = 'Il vincitore è il computer';
+}
