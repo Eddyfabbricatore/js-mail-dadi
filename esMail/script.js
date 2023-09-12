4094a098f506f0f396e0d2971df6e0755962ac8a
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-09-12 15:35:05 +0200

    js [add] accesso [add] ciclo for [add] if confronto

--- esMail/script.js
+++ esMail/script.js
@@ -8,6 +8,20 @@ const emailInserita = document.getElementById('InputEmail1').value;
 
 const btn = document.getElementById('btn1');
 
+const accesso = document.querySelector('h2');
+
 btn.addEventListener('click', function(){
-  // console.log(emailInserita);
+  console.log(emailInserita);
+
+  for(let i = 0; i < emailList.length; i++){
+    if(emailInserita === emailList[i]){
+      console.log('presente');
+
+      accesso.innerHTML = 'Accesso consentito';
+    }else{
+      console.log('Non presente');
+
+      accesso.innerHTML = 'Accesso non consentito';
+    }
+  }
 });
