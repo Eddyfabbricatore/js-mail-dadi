87ae5ab9c556625811d891b0bb5f911c7e120e8a
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-09-12 15:45:10 +0200

    [add] comments

--- esMail/script.js
+++ esMail/script.js
@@ -11,14 +11,20 @@ const btn = document.getElementById('btn1');
 const accesso = document.querySelector('h2');
 
 btn.addEventListener('click', function(){
-  console.log(emailInserita);
+  // console.log(emailInserita);
 
   for(let i = 0; i < emailList.length; i++){
     if(emailInserita === emailList[i]){
+      // console.log(emailInserita);
+      // console.log(emailList);
       console.log('presente');
 
       accesso.innerHTML = 'Accesso consentito';
+
+      break;
     }else{
+      // console.log(emailInserita);
+      // console.log(emailList);
       console.log('Non presente');
 
       accesso.innerHTML = 'Accesso non consentito';
