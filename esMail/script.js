aa0dfff99c042cdca5d6cee9845829fea84df4e6
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-09-12 15:21:08 +0200

    js [add] emailList [add] emailInserita [add] btn listener

---
+++ esMail/script.js
@@ -0,0 +1,13 @@
+const emailList = [
+  'user@gmail.com',
+  'personaluser@gmail.com',
+  'booleanuser@gmail.com'
+];
+
+const emailInserita = document.getElementById('InputEmail1').value;
+
+const btn = document.getElementById('btn1');
+
+btn.addEventListener('click', function(){
+  // console.log(emailInserita);
+});
